cbfcda07a238407cb678174122d67b4d619c9c0a
JackyC99 <chenjacky838@gmail.com>
2025-04-02 15:21:42 -0400

    Replaced the thread with coroutine and retain the same functionality

--- app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
+++ app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
@@ -2,16 +2,13 @@ package edu.temple.coroutineconversion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import kotlinx.coroutines.*
 import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
-
-    //TODO (Refactor to replace Thread code with coroutines)
 
     private val cakeImageView: ImageView by lazy {
         findViewById(R.id.imageView)
@@ -21,24 +18,29 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.currentTextView)
     }
 
-    val handler = Handler(Looper.getMainLooper(), Handler.Callback {
-
-        currentTextView.text = String.format(Locale.getDefault(), "Current opacity: %d", it.what)
-        cakeImageView.alpha = it.what / 100f
-        true
-    })
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.revealButton).setOnClickListener{
-            Thread{
-                repeat(100) {
-                    handler.sendEmptyMessage(it)
-                    Thread.sleep(40)
-                }
-            }.start()
+        findViewById<Button>(R.id.revealButton).setOnClickListener {
+            coroutineScope.launch {
+                revealImage()
+            }
         }
+    }
+
+    private suspend fun revealImage() {
+        for (i in 0..99) {
+            currentTextView.text = String.format(Locale.getDefault(), "Current opacity: %d", i)
+            cakeImageView.alpha = i / 100f
+            delay(40L)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        coroutineScope.cancel()
     }
 }
